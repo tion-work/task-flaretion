977766249dd3c68e173e374caf3555dcd81f64eb
WENBIN.LI <webbleen@gmail.com>
2025-08-22 15:18:04 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080';
     return [
       {
         source: '/api/:path*',
-        destination: process.env.NEXT_PUBLIC_API_URL + '/api/:path*',
+        destination: `${apiUrl}/api/:path*`,
       },
     ];
   },

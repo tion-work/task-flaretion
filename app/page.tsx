7d5c796179ab53e89344e8452282f6b44cb8c2ed
WENBIN.LI <webbleen@gmail.com>
2025-08-22 17:32:58 +0900

    !showAppName

--- app/page.tsx
+++ app/page.tsx
@@ -182,7 +182,7 @@ export default function HomePage() {
         </div>
 
         {/* Footer */}
-        <Copyright showAppName={true} />
+        <Copyright showAppName={false} />
       </div>
     </div>
   )
